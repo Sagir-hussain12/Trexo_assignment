2ab8c5fe9b4238fb446e01dce626698175787180
Sagir-hussain12 <sagiransary18@gmail>
2025-05-02 15:09:17 +0530

    remove carousel

--- src/components/UserInfo/UserList.jsx
+++ src/components/UserInfo/UserList.jsx
@@ -1,56 +1,22 @@
-import React from 'react';
-import UserCard from './UserCard'; 
-import Slider from 'react-slick';
-import './user.css'; 
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
+import React from 'react'
+import UserCard from './UserCard';
 
-const UserList = ({ users, isLoading, error }) => {
-  // Slider settings
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          initialSlide: 1
-        }
-      }
-    ]
-  };
-
-  if (isLoading) {
-    return <div className="loading-message">Loading...</div>;
-  }
-
-  if (error) {
-    return <div className="error-message">Failed to fetch users.</div>;
-  }
-
-  return (
-    <div className="user-list-container">
-      <Slider {...settings}>
-      {users.map(user => (
+const UserList =({ users, isLoading, error }) => {
+    if (isLoading) {
+      return <div className="loading-message">Loading...</div>;
+    }
+  
+    if (error) {
+      return <div className="error-message">Failed to fetch users.</div>;
+    }
+  
+    return (
+      <div className="user-list">
+        {users.map(user => (
           <UserCard key={user.id} user={user} />
         ))}
-      </Slider>
-    </div>
-  );
-};
+      </div>
+    );
+  };
 
-export default UserList;
+export default UserList
